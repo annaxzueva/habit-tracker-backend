9a1327b89816e4566c5f9630c48489955458cf9f
Anna Zueva <anna.kitty.zueva@gmail.com>
2026-06-28 19:42:03 +0300

    Добавлен новый адрес Netlify в CORS

--- src/main/java/com/example/habit_tracker_backend/HabitTrackerBackendApplication.java
+++ src/main/java/com/example/habit_tracker_backend/HabitTrackerBackendApplication.java
@@ -22,7 +22,8 @@ public class HabitTrackerBackendApplication {
 						.allowedOrigins(
 								"http://localhost:3000",
 								"http://localhost:5500",
-								"https://beamish-nougat-01aa70.netlify.app"
+								"https://amazing-kheer-56543d.netlify.app"
+								//"https://beamish-nougat-01aa70.netlify.app"
 						)
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
